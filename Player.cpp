cab2090762b7bc144fd5deba728f2a277f9624cc
MAMADOU COULIBALY <152911765+Mdc1960@users.noreply.github.com>
2025-02-07 16:51:09 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,16 @@
 #include "Player.hpp"
 
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
 Player::Player()
 {
 	id = 0;
+}
+
+Player::printId(){
+	cout << "Player Id is : " << id << endl;
 }
