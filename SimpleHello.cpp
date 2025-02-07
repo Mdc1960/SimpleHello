73e0257566812954b153ea714bedffd31dd7926c
Mdc1960 <mamadou.coulibaly@etu.univ-tous.fr>
2025-02-07 16:58:36 +0100

    message

--- SimpleHello.cpp
+++ SimpleHello.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "Player.hpp"
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    Player p = Player();
+
+    p.printId();
+
+
+    return 0;
 }
